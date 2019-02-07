985e948f4ed77d58f71d53711bad8dfd1193de8c
njdevengine <newjerseydevelopment@gmail.com>
2019-02-07 18:40:41 -0500

    Update pandas-file-creation-by-array2.py

--- pandas-file-creation-by-array2.py
+++ pandas-file-creation-by-array2.py
@@ -4,6 +4,6 @@ array_length = len(myarray)
 path = "sourcefile.csv"
 for i in range(0,array_length):
     df = pd.read_csv(path,encoding = "UTF-8")
-    #only saves values that match your array member, can be switched to !=
+    #only saves values that match your array member, can be switched to != to filter out array value
     df = df[df.headertobefiltered == myarray[i]]
     df.to_csv(str(myarray[i])+".csv",encoding = "utf-8", index =False, header = True)
